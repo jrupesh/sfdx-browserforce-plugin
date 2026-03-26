587784831cb29568821d95d0a5dec2c5a676eac6
Rupesh J <j.rupesh@gmail.com>
2026-03-26 22:30:55 +0530

    Add version details

--- src/plugins/logout-login-as-user/index.ts
+++ src/plugins/logout-login-as-user/index.ts
@@ -1,7 +1,7 @@
 import type { SalesforceUrlPath } from '../../browserforce.js';
 import { BrowserforcePlugin } from '../../plugin.js';
 
-const LOGOUT_URL: SalesforceUrlPath = '/secur/logout.jsp';
+const LOGOUT_URL: SalesforceUrlPath = `/secur/logout.jsp?retURL=${encodeURIComponent('/setup/forcecomHomepage.apexp')}` as SalesforceUrlPath;
 
 export type Config = Record<string, never>;
 
@@ -11,7 +11,7 @@ export class LogoutLoginAsUser extends BrowserforcePlugin {
   }
 
   public async apply(): Promise<void> {
-    await using page = await this.browserforce.openPage(LOGOUT_URL);
+    await using page = await this.browserforce.openPage(LOGOUT_URL as SalesforceUrlPath);
     await page.waitForLoadState('load');
   }
 }
