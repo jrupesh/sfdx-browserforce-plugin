4218ba42c3b12ac19cac800d538570a77a5d4828
Rupesh J <j.rupesh@gmail.com>
2026-05-04 18:48:18 +0530

    feat: Handle maintenance page on login

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -3,6 +3,7 @@ import { type Page } from 'playwright';
 import { waitForPageErrors } from '../browserforce.js';
 
 const POST_LOGIN_PATH = '/setup/forcecomHomepage.apexp';
+const MAINTENANCE_PATH = '/msg/maintenanceandavailable.jsp';
 
 export class LoginPage {
   private page: Page;
@@ -15,7 +16,23 @@ export class LoginPage {
     const org = await Org.create({ connection });
     const frontDoorUrl = await org.getFrontDoorUrl(POST_LOGIN_PATH);
     await this.page.goto(frontDoorUrl);
-    await Promise.race([this.page.waitForURL((url) => url.pathname === POST_LOGIN_PATH), waitForPageErrors(this.page)]);
+
+    while (true) {
+      const destination = await Promise.race([
+        this.page.waitForURL((url) => url.pathname === POST_LOGIN_PATH).then(() => 'postLogin' as const),
+        this.page.waitForURL((url) => url.pathname === MAINTENANCE_PATH).then(() => 'maintenance' as const),
+        waitForPageErrors(this.page).then(() => {
+          throw new Error('Navigation reached an error state without an error message');
+        }),
+      ]);
+
+      if (destination === 'postLogin') {
+        break;
+      }
+
+      await this.page.locator('form p.input a.continue').click();
+    }
+
     return this;
   }
 }
